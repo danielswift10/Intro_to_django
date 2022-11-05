660a4a700183854638e57b98be9cf1ea2b0c3dbb
danielswift10 <ojeyomi.daniel@gmail.com>
2022-11-05 07:16:15 +0100

    fixed

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .serializers import ArtisteSerializers, SongSerializers, LyricSerializers
+from rest_framework import viewsets
+from .models import Artiste, Song, Lyric
 
-# Create your views here.
+
+class ArtisteViewsets(viewsets.ModelViewSet):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializers
+
+
+class SongViewsets(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializers
